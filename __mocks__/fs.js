7d762f981236d0785009f00b5aac7110d2a80e70
Keith Dahlby <keith@newbo.co>
2019-05-22 17:27:10 -0500

    Make mock less stupid

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -3,8 +3,8 @@
 module.exports = exports = {};
 
 exports.readFile = (file, cb) => {
-  if( file.match(/bad/i) ) {
-    cb('Invalid File');
+  if(!file || file.match(/bad/i) ) {
+    cb(`${file} has an error`);
   }
   else {
     cb(undefined, new Buffer('File Contents'));
