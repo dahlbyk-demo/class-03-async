abb82dce824a6b00cf864628aa296a92fbd2fc16
Keith Dahlby <keith@newbo.co>
2019-05-23 12:01:01 -0500

    Convert to promises

--- __tests__/copy-file-promise.test.js
+++ __tests__/copy-file-promise.test.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const fs = require('fs');
+const util = require('util');
+
+const fsAccess = util.promisify(fs.access);
+
 const EOL = require('os').EOL;
 
 const copyFile = require('../copy-file-promise');
@@ -11,54 +15,42 @@ const generateFileToWrite = () => {
   return `${__dirname}/../files/output/${random}.txt`;
 };
 
-describe('Copy File Module', () => {
-  it('calls callback with error for missing readFile', done => {
-    let result = copyFile('missingFile', 'fileToWrite', (err, data) => {
-      expect(err).toBeDefined();
-      done();
-    });
+describe('Copy File Promise Module', () => {
+  it('calls callback with error for missing readFile', () => {
+    var copyFileResultPromise = copyFile('missingFile', 'fileToWrite');
 
-    expect(result).toBeUndefined();
+    return expect(copyFileResultPromise)
+      .rejects.toBeDefined();
   });
 
-  it('calls callback with data from readFile', done => {
+  it('calls callback with data from readFile', () => {
     let fileToWrite = generateFileToWrite();
-    copyFile(fileToRead, fileToWrite, (err, data) => {
-      expect(err).toBeFalsy();
-      expect(data).toBeDefined();
-      // console.log(data); // shows newline, so either trim or compare with EOL
-      expect(data.toString().trim()).toBe('File 2 Contents');
-      expect(data.toString()).toBe(`File 2 Contents${EOL}`);
-      done();
-    });
+
+    return copyFile(fileToRead, fileToWrite)
+      .then(data => {
+        expect(data).toBeDefined();
+        // console.log(data); // shows newline, so either trim or compare with EOL
+        expect(data.toString().trim()).toBe('File 2 Contents');
+        expect(data.toString()).toBe(`File 2 Contents${EOL}`);
+      });
   });
 
-  it('creates fileToWrite', done => {
+  it('creates fileToWrite', () => {
     let fileToWrite = generateFileToWrite();
     console.log(fileToWrite);
 
-    copyFile(fileToRead, fileToWrite, (err, data) => {
-      expect(err).toBeFalsy();
-      expect(data).toBeDefined();
+    return copyFile(fileToRead, fileToWrite)
+      .then((data) => {
+        expect(data).toBeDefined();
 
-      fs.access(fileToWrite, fs.constants.F_OK, (err) => {
-        if (err) {
-          throw err;
-        } 
-        else {
-          done();
-        }
+        return fsAccess(fileToWrite, fs.constants.F_OK);
       });
-    });
   });
 
-  it('fails on fileToWrite into missing directory', done => {
+  it('fails on fileToWrite into missing directory', () => {
     var badFileToWrite = `${__dirname}/this-is-missing/test.txt`;
 
-    copyFile(fileToRead, badFileToWrite, (err, data) => {
-      expect(data).toBeUndefined();
-      expect(err).toBeTruthy();
-      done();
-    });
+    return expect(copyFile(fileToRead, badFileToWrite))
+      .rejects.toBeDefined();
   });
 });
--- copy-file-promise.js
+++ copy-file-promise.js
@@ -1,22 +1,15 @@
 'use strict';
 
 const fs = require('fs');
+const util = require('util');
 
-module.exports = function(fileToRead, fileToWrite, callback) {
-  fs.readFile(
-    fileToRead,
-    (err, data) => {
-      if (err) {
-        callback(err);
-      }
-      else {
-        fs.writeFile(fileToWrite, data, (err) => {
-          if (err) {
-            return callback(err);
-          }
+const fsReadFile = util.promisify(fs.readFile);
+const fsWriteFile = util.promisify(fs.writeFile);
 
-          callback(undefined, data);
-        });
-      }
+module.exports = function(fileToRead, fileToWrite) {
+  return fsReadFile(fileToRead)
+    .then(data => {
+      return fsWriteFile(fileToWrite, data)
+        .then(() => Promise.resolve(data));
     });
 };
